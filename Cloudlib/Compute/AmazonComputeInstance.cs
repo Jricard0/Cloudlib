386c13d0161ea93f28d511b3a741b5e7e48dc4b3
Joao Silva <jroxs@hotmail.com>
2022-11-17 20:48:47 -0300

    add (aws): virtual machine service

---
+++ Cloudlib/Compute/AmazonComputeInstance.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Cloudlib.Compute.Services;
+using Cloudlib.Models;
+
+namespace Cloudlib.Compute
+{
+    public class AmazonComputeInstance : IComputeInstance
+    {
+        public AmazonComputeInstance()
+        {
+        }
+
+        public string GetName()
+        {
+            return "Amazon AWS Compute Instances";
+        }
+
+        public List<VirtualMachine> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<VirtualMachine> List(string location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<VirtualMachine>> ListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<VirtualMachine>> ListAsync(string location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Start(string name, string zone)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Stop(string name, string zone)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
